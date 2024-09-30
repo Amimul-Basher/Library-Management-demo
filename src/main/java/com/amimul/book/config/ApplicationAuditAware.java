f49dcd2f44c56c1cf90199e5798ee29b45743c01
amimul <mdbasher.web@gmail.com>
2024-09-30 09:09:36 -0400

    Implementation: Application Audit Aware   1. Creating Application Audit Aware class   2. Create Bean of that class   3. Inject that Bean method to main Application class through annotation.

---
+++ src/main/java/com/amimul/book/config/ApplicationAuditAware.java
@@ -0,0 +1,21 @@
+package com.amimul.book.config;
+
+import com.amimul.book.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication instanceof AnonymousAuthenticationToken || authentication ==null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+        User user = (User) authentication.getPrincipal();
+        return Optional.ofNullable(user.getId());
+    }
+}
